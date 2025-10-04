feaf41e059f88b34e93f2254681f5f7e28477ffb
Manuel Wardeh <wardeh@weapptec.de>
2025-10-04 12:09:41 +0000

    Changes!

--- app/design/frontend/bv24/luma_child/web/js/search-fix.js
+++ app/design/frontend/bv24/luma_child/web/js/search-fix.js
@@ -16,8 +16,67 @@ define([
             console.log('Create called');
             originalCreate.apply(this, arguments);
 
-            // Add mutation observer to watch for autocomplete changes
             var self = this;
+
+            // WICHTIG: Entferne ALLE blur Event Handler die von den Original-Dateien hinzugefügt wurden
+            this.element.off('blur');
+
+            // Füge eigenen blur Handler hinzu
+            this.element.on('blur', $.proxy(function () {
+                this.element.on('blur', $.proxy(function () {
+                if (!this.searchLabel.hasClass('active')) {
+                    return;
+                }
+
+                setTimeout($.proxy(function () {
+                    if (this.autoComplete.is(':hidden')) {
+                        this.setActiveState(false);
+                    } else {
+                        this.element.trigger('focus');
+                    }
+                    //this.autoComplete.hide();
+                    this._updateAriaHasPopup(false);
+                }, this), 250);
+            }, this));
+            }, this));
+
+            // Click außerhalb von Suchfeld und Autocomplete erkennen
+            var touchStartY = 0;
+            var touchMoved = false;
+
+            $(document).on('touchstart.searchclose', function(e) {
+                touchStartY = e.touches[0].clientY;
+                touchMoved = false;
+            });
+
+            $(document).on('touchmove.searchclose', function(e) {
+                if (Math.abs(e.touches[0].clientY - touchStartY) > 10) {
+                    touchMoved = true;
+                }
+            });
+
+            $(document).on('click.searchclose touchend.searchclose', function(e) {
+                // Bei touchend: Nur schließen wenn NICHT gescrollt wurde
+                if (e.type === 'touchend' && touchMoved) {
+                    return;
+                }
+
+                var $target = $(e.target);
+                var isSearchField = $target.closest(self.element).length > 0;
+                var isAutocomplete = $target.closest(self.autoComplete).length > 0;
+                var isSearchForm = $target.closest(self.searchForm).length > 0;
+
+                if (!isSearchField && !isAutocomplete && !isSearchForm) {
+                setTimeout($.proxy(function () {
+                    self.autoComplete.hide();
+                    self._updateAriaHasPopup(false);
+                    self.setActiveState(false);
+                }, this), 250);
+
+                }
+            });
+
+            // Add mutation observer to watch for autocomplete changes
             if (this.autoComplete && this.autoComplete.length) {
                 var observer = new MutationObserver(function(mutations) {
                     mutations.forEach(function(mutation) {
@@ -59,24 +118,6 @@ define([
                     console.log('Show all link added successfully');
                 }
             }
-        };
-
-        // Override the _blur method
-        target.prototype._blur = function() {
-            this.element.on('blur', $.proxy(function () {
-                if (!this.searchLabel.hasClass('active')) {
-                    return;
-                }
-                // First hide autocomplete immediately
-                this.autoComplete.hide();
-                this._updateAriaHasPopup(false);
-
-                // Then handle focus removal with short delay
-                setTimeout($.proxy(function () {
-                    $('#search').trigger('blur');
-                    this.setActiveState(false);
-                }, this), 100);
-            }, this));
         };
 
         return target;
